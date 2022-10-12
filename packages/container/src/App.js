0e8d00cd4d65d954496a4be10676b1065e11f809
Ben Singer <ben.singer@sds.co.uk>
2022-10-12 17:10:12 +0100

    updated typo

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,8 +3,8 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
   return (
-    <div className=''>
-      <h1>Hi there, how are you!?!?!?</h1>
+    <div className='tetst'>
+      <h1>Hi there, how are you?</h1>
       <hr />
       <MarketingApp />
     </div>
